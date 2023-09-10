386b1a12091d5358e8a4ae3706c123c8ef3e4234
Chung Nguyen <zrmedia9@gmail.com>
2023-09-10 11:28:05 +0700

    Update code

--- lib/modules/novel/providers/madara-v0.1.dart
+++ lib/modules/novel/providers/madara-v0.1.dart
@@ -1,59 +1,59 @@
-
+import 'package:html/dom.dart';
 import 'package:html/parser.dart';
 import 'package:http/http.dart' as http;
 import 'package:my_novel_reader_crawl/models/novel.dart';
 import 'package:xpath_selector_html_parser/xpath_selector_html_parser.dart';
 
 final statusList = [
-    {
-      "OnGoing": 0,
-      "Продолжается": 0,
-      "Updating": 0,
-      "Em Lançamento": 0,
-      "Em lançamento": 0,
-      "Em andamento": 0,
-      "Em Andamento": 0,
-      "En cours": 0,
-      "Ativo": 0,
-      "Lançando": 0,
-      "Đang Tiến Hành": 0,
-      "Devam Ediyor": 0,
-      "Devam ediyor": 0,
-      "In Corso": 0,
-      "In Arrivo": 0,
-      "مستمرة": 0,
-      "مستمر": 0,
-      "En Curso": 0,
-      "En curso": 0,
-      "Emision": 0,
-      "En marcha": 0,
-      "Publicandose": 0,
-      "En emision": 0,
-      "连载中": 0,
-      "Completed": 1,
-      "Completo": 1,
-      "Completado": 1,
-      "Concluído": 1,
-      "Concluido": 1,
-      "Finalizado": 1,
-      "Terminé": 1,
-      "Hoàn Thành": 1,
-      "مكتملة": 1,
-      "مكتمل": 1,
-      "已完结": 1,
-      "On Hold": 2,
-      "Pausado": 2,
-      "En espera": 2,
-      "Canceled": 3,
-      "Cancelado": 3,
-    }
-  ];
+  {
+    "OnGoing": 0,
+    "Продолжается": 0,
+    "Updating": 0,
+    "Em Lançamento": 0,
+    "Em lançamento": 0,
+    "Em andamento": 0,
+    "Em Andamento": 0,
+    "En cours": 0,
+    "Ativo": 0,
+    "Lançando": 0,
+    "Đang Tiến Hành": 0,
+    "Devam Ediyor": 0,
+    "Devam ediyor": 0,
+    "In Corso": 0,
+    "In Arrivo": 0,
+    "مستمرة": 0,
+    "مستمر": 0,
+    "En Curso": 0,
+    "En curso": 0,
+    "Emision": 0,
+    "En marcha": 0,
+    "Publicandose": 0,
+    "En emision": 0,
+    "连载中": 0,
+    "Completed": 1,
+    "Completo": 1,
+    "Completado": 1,
+    "Concluído": 1,
+    "Concluido": 1,
+    "Finalizado": 1,
+    "Terminé": 1,
+    "Hoàn Thành": 1,
+    "مكتملة": 1,
+    "مكتمل": 1,
+    "已完结": 1,
+    "On Hold": 2,
+    "Pausado": 2,
+    "En espera": 2,
+    "Canceled": 3,
+    "Cancelado": 3,
+  }
+];
 
 getPopularNovel(String baseUrl, int page) async {
   try {
     final url = "$baseUrl/novel/page/$page/?m_orderby=views";
     final response = await http.get(Uri.parse(url));
-    if(response.statusCode != 200) {
+    if (response.statusCode != 200) {
       return null;
     }
     final html = parse(response.body).documentElement!;
@@ -67,7 +67,8 @@ getPopularNovel(String baseUrl, int page) async {
     var newNames = names.attrs;
 
     urls.attrs.asMap().forEach((index, url) {
-      _novels.add(NovelUrlInfo(name: newNames[index]!, url: url!, image: newImages[index]!));
+      _novels.add(NovelUrlInfo(
+          name: newNames[index]!, url: url!, image: newImages[index]!));
     });
     return _novels;
   } catch (e) {
@@ -79,17 +80,43 @@ getNovelDetail(NovelUrlInfo novelUrlInfo) async {
   try {
     print(novelUrlInfo.url);
     final response = await http.get(Uri.parse(novelUrlInfo.url));
-    if(response.statusCode != 200) {
+    if (response.statusCode != 200) {
       return null;
     }
     var html = parse(response.body).documentElement!;
+    final novelId =
+        html.querySelector("#manga-chapters-holder")!.attributes["data-id"];
     var author = html.querySelector(".post-title h1")!.text;
-    var imageUrl = html.querySelector(".summary_image img[data-src]")!.attributes["data-src"];
+    var imageUrl = html
+        .querySelector(".summary_image img[data-src]")!
+        .attributes["data-src"];
     var status = html.querySelectorAll("div.summary-content");
+    List<Element> genres = html.querySelectorAll("div.genres-content a");
+    var newGenres = genres.map((genre) {
+      return genre.text;
+    }).toList();
 
+    var baseUrl = "https://boxnovel.com/";
+
+    final headers = {
+      "Referer": baseUrl,
+      "Content-Type": "application/x-www-form-urlencoded",
+      "X-Requested-With": "XMLHttpRequest"
+    };
+
+    final url =
+        "${baseUrl}wp-admin/admin-ajax.php?action=manga_get_chapters&manga=$novelId";
+
+    var chapterList = await http.get(Uri.parse(url), headers: headers);
+    print(chapterList);
+    if (chapterList.statusCode == 400) {
+      final urlP = "${novelUrlInfo.url}ajax/chapters";
+      chapterList = await http.get(Uri.parse(urlP), headers: headers);
+      print(chapterList);
+    } else {}
     print(author);
     print(imageUrl);
-    print(status);
+    print(newGenres);
   } catch (e) {
     print(e.toString());
   }
